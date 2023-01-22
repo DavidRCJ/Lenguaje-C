d1c1c3842d696f5aa53c18b9c928b04514ca120f
David <davidricardocruzjuarez@gmail.com>
2023-01-21 21:03:51 -0600

    practica de operadores .c

---
+++ linkedin_learning/practica_operaciones.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+    int main(){
+        int oprando1;
+        int operando2;
+
+        printf("Ingresa los valores 1 \n");
+        scanf("%d",&oprando1);
+
+        printf("Ingresa los valores 2\n");
+        scanf("%d",&operando2);
+
+        printf("resultados \n");
+        printf("Suma = %d \n", oprando1 + operando2);
+        printf("Resta = %d \n", oprando1 - operando2);
+        printf("Multiplicacion = %d \n", oprando1 * operando2);
+        printf("Division = %d \n", oprando1 / operando2);
+        printf("Mod = %d \n", oprando1 % operando2);
+
+
+    }
